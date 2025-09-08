38ff1bbd375bb7a9069aaa0dfe3ff98ed04d2a91
Jhoc <johnharold.cruda@gmail.com>
2025-09-08 15:40:47 +0800

    hamburger

--- js/script.js
+++ js/script.js
@@ -15,4 +15,11 @@
                         img.style.opacity = 1;
                     }
                 }, 400);
+
             }
+            const menuToggle = document.getElementById("menu-toggle");
+            const navLinks = document.getElementById("nav-links");
+
+                menuToggle.addEventListener("click", () => {
+                    navLinks.classList.toggle("show");
+                });
