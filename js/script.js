f3ec73abae2c4a2a7ce3f9e450ec61f6ab066396
Jhoc <johnharold.cruda@gmail.com>
2025-09-08 15:00:08 +0800

    update

---
+++ js/script.js
@@ -0,0 +1,18 @@
+ function switchProfilePic() {
+                const img = document.getElementById('profile-pic');
+                img.style.opacity = 0;
+                setTimeout(() => {
+                    if (img.src.includes('avatar.jpg')) {
+                        img.src = 'images/profile.jpg';
+                    } else {
+                        img.src = 'images/avatar.jpg';
+                    }
+                    img.onload = () => {
+                        img.style.opacity = 1;
+                    };
+                    // For cached images, trigger opacity immediately
+                    if (img.complete) {
+                        img.style.opacity = 1;
+                    }
+                }, 400);
+            }
