a7bf9c5b28ebbbee5cc8ccd2968d1746e00a2bb0
Jhoc <johnharold.cruda@gmail.com>
2025-09-09 15:51:07 +0800

    update file and delete template folder

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
- function switchProfilePic() {
-                const img = document.getElementById('profile-pic');
-                img.style.opacity = 0;
-                setTimeout(() => {
-                    if (img.src.includes('avatar.jpg')) {
-                        img.src = 'images/profile.jpg';
-                    } else {
-                        img.src = 'images/avatar.jpg';
-                    }
-                    img.onload = () => {
-                        img.style.opacity = 1;
-                    };
-                  
-                    if (img.complete) {
-                        img.style.opacity = 1;
-                    }
-                }, 400);
+function switchProfilePic() {
+    const img = document.getElementById('profile-pic');
+    img.style.opacity = 0;
 
-            }
+    setTimeout(() => {
+        if (img.src.includes("avatar.jpg")) {
+            img.src = "images/profile.jpg";
+        } else {
+            img.src = "images/avatar.jpg";
+        }
+
+        img.onload = () => {
+            img.style.opacity = 1;
+        };
+
+        if (img.complete) {
+            img.style.opacity = 1;
+        }
+    }, 400);
+}
+
             const menuToggle = document.getElementById("menu-toggle");
             const navLinks = document.getElementById("nav-links");
 
